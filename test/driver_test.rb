e5ada21a176dc9d6eeed479a660eab4faf2168ad
Edward Price <ted.price@gmail.com>
2012-11-26 01:52:42 -0600

    tired hack job to get input box for kanban board, damn you ID channel!

--- test/driver_test.rb
+++ test/driver_test.rb
@@ -7,12 +7,28 @@ require "./lib/kan_ban_solo"
 WIDTH   = 20
 HEIGHT  = 5
 
+def get_input(win, text_box)
+  win.setpos(1,1)
+  win.box("|", "-")
+  win.addstr("[text]: " + text_box.text)
+  win.setpos(1,9)
+  win.refresh
+
+  Curses.echo
+  text_box.text = win.getstr
+  Curses.noecho
+
+  win.clear
+  win.refresh
+end
 
 begin
   Curses.init_screen
   Curses.noecho
 
-  win  = Curses::Window.new(Curses.lines-2, Curses.cols - 2, 1, 1)
+  input_window = Curses::Window.new(3, 0, Curses.lines-3, 1)
+
+  win  = Curses::Window.new(Curses.lines-5, Curses.cols - 2, 1, 1)
   win.box("|", "-")
   win.refresh
   win.keypad(true)
@@ -37,6 +53,9 @@ begin
     when Curses::Key::RIGHT then board.col += 1
     when Curses::Key::UP    then board.row -= 1
     when Curses::Key::DOWN  then board.row += 1
+    when 10
+      get_input(input_window, board.current)
+      board.redraw
     when "u", "d"
       m  = key.eql?("d") ? 1 : -1
       board.move_row(m)
